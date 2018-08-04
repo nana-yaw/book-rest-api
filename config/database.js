84bad96594d23e80c4cd29c350b08b7708af202a
eddiemania <edward.osei.nyarko@gmail.com>
2018-08-04 23:45:20 +0000

    Modify db config for heroku postgres

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
-'use strict'
+"use strict";
 
-const Env = use('Env')
-const Helpers = use('Helpers')
+const Env = use("Env");
+const Helpers = use("Helpers");
+const Url = require("url-parse");
+const DATABASE_URL = new Url(Env.get("DATABASE_URL"));
 
 module.exports = {
   /*
@@ -13,7 +15,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get("DB_CONNECTION", "sqlite"),
 
   /*
   |--------------------------------------------------------------------------
@@ -27,12 +29,14 @@ module.exports = {
   |
   */
   sqlite: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
+      filename: Helpers.databasePath(
+        `${Env.get("DB_DATABASE", "development")}.sqlite`
+      )
     },
     useNullAsDefault: true,
-    debug: Env.get('DB_DEBUG', false)
+    debug: Env.get("DB_DEBUG", false)
   },
 
   /*
@@ -46,15 +50,15 @@ module.exports = {
   |
   */
   mysql: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get("DB_HOST", "localhost"),
+      port: Env.get("DB_PORT", ""),
+      user: Env.get("DB_USER", "root"),
+      password: Env.get("DB_PASSWORD", ""),
+      database: Env.get("DB_DATABASE", "adonis")
     },
-    debug: Env.get('DB_DEBUG', false)
+    debug: Env.get("DB_DEBUG", false)
   },
 
   /*
@@ -67,15 +71,28 @@ module.exports = {
   | npm i --save pg
   |
   */
+  //DB connection for local development
+  // pg: {
+  //   client: "pg",
+  //   connection: {
+  //     host: Env.get("DB_HOST", "localhost"),
+  //     port: Env.get("DB_PORT", ""),
+  //     user: Env.get("DB_USER", "root"),
+  //     password: Env.get("DB_PASSWORD", ""),
+  //     database: Env.get("DB_DATABASE", "adonis")
+  //   },
+  //   debug: Env.get("DB_DEBUG", false)
+  // }
+
+  //DB connection for Heroku deployment
   pg: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    },
-    debug: Env.get('DB_DEBUG', false)
+      host: Env.get("DB_HOST", DATABASE_URL.hostname),
+      port: Env.get("DB_PORT", DATABASE_URL.port),
+      user: Env.get("DB_USER", DATABASE_URL.username),
+      password: Env.get("DB_PASSWORD", DATABASE_URL.password),
+      database: Env.get("DB_DATABASE", DATABASE_URL.pathname.substr(1))
+    }
   }
-}
+};
