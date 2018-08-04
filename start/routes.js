a41805b0330353bce7d241b0197e633a75b02ee6
eddiemania <edward.osei.nyarko@gmail.com>
2018-08-04 11:04:55 +0000

    All basic HTTP CRUD operations on the 'Book' model completed

--- start/routes.js
+++ start/routes.js
@@ -16,11 +16,12 @@
 const Route = use("Route");
 const Book = use("App/Models/Book");
 
-Route.get("/", ({ request }) => {
-  return { greeting: "Hello world in JSON" };
-});
+// Route.get("/", ({ request }) => {
+//   return { greeting: "Hello world in JSON" };
+// });
 
 Route.group(() => {
+  //Implement handling of empty responses
   Route.post("books", async ({ request, response }) => {
     // https://gist.github.com/nanotaboada/6396437
 
@@ -54,11 +55,61 @@ Route.group(() => {
 
   Route.get("books", async ({ response }) => {
     let books = await Book.all();
+
+    if (!books) {
+      const error = { message: "No books found at this time!" };
+      return response.status(404).json(error);
+    }
+
     return response.json(books);
   });
 
   Route.get("books/:id", async ({ params, response }) => {
     const book = await Book.find(params.id);
+
+    if (!book) {
+      const error = { message: "Requested book not found at this time!" };
+      return response.status(404).json(error);
+    }
+
     return response.json(book);
+  });
+
+  Route.put("books/:id", async ({ params, request, response }) => {
+    const bookInfo = request.only([
+      "isbn",
+      "title",
+      "subtitle",
+      "author",
+      "published",
+      "publisher",
+      "pages",
+      "description",
+      "website"
+    ]);
+    const book = await Book.find(params.id);
+    book.isbn = bookInfo.isbn;
+    book.title = bookInfo.title;
+    book.subtitle = bookInfo.subtitle;
+    book.author = bookInfo.author;
+    book.published = bookInfo.published;
+    book.publisher = bookInfo.publisher;
+    book.pages = bookInfo.pages;
+    book.description = bookInfo.description;
+    book.website = bookInfo.website;
+
+    await book.save();
+
+    return response.status(200).json(book);
+  });
+
+  Route.delete("books/:id", async ({ params, request, response }) => {
+    const book = await Book.find(params.id);
+    if (!book) {
+      const error = { message: "Requested book not found at this time!" };
+      return response.status(404).json(error);
+    }
+    await book.delete();
+    return response.status(200).json([{ message: "Book removed!" }]);
   });
 }).prefix("api/v1");
